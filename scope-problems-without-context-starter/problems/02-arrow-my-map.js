653f877d30b40e263f9d1dbd5dc94ca65959a047
Justin <just2rules@gmail.com>
2021-02-18 14:26:13 -0800

    first 3 problems

--- scope-problems-without-context-starter/problems/02-arrow-my-map.js
+++ scope-problems-without-context-starter/problems/02-arrow-my-map.js
@@ -1,7 +1,7 @@
 /***********************************************************************
 Write a function using fat arrow syntax named `arrowMyMap` that accepts an array
 and a callback as arguments. The function will return an array of new elements
-obtained by calling the callback on each element of the array, passing in the 
+obtained by calling the callback on each element of the array, passing in the
 element. Assign the below function to a variable using the const keyword.
 
 Do not use the built in Array#map - use Array#forEach for iteration.
@@ -18,7 +18,11 @@ console.log(result2);   // [ 'RUN!', 'FORREST!' ]
 
 ***********************************************************************/
 
-// your code here!
+const arrowMyMap = (arr, cb) => {
+  let map = [];
+  arr.forEach( el => map.push(cb(el)) );
+  return map;
+};
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
